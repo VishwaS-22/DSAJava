98c69db1950dfd0ad399685477812808cb9b79d2
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2022-09-03 13:53:39 +0530

    Loops (Conditionals)

---
+++ JavaCodes/loops.java
@@ -0,0 +1,46 @@
+package vishwa;
+
+public class loops {
+    public static void main(String[] args) {
+        /*
+        print 1 to 5
+        for (int v = 1; v <=5 ; v++) {
+            System.out.println(v);
+        }
+        */
+        //print 1 to n
+
+        /*
+        Scanner in=new Scanner(System.in);
+        int v=in.nextInt();
+        for ( int num = 1; num <=v ; num+=2) {
+            System.out.println(num);
+        }
+        */
+
+        /*
+        int v=1;
+        while(v<=5){
+            System.out.println(v);
+            v+=1;
+        }
+        */
+
+        /*
+        Scanner in=new Scanner(System.in);
+        int s=in.nextInt();
+        int v=1;
+        while(v<=s){
+            System.out.println(v);
+            v++;
+        }
+        */
+
+        int n=1;
+        do{
+            System.out.println(n);
+            n++;
+        }while(n<=5);
+
+    }
+}

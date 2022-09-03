1615960edae792be991eba0c9cc7f83425f23c51
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2022-09-03 14:01:40 +0530

    lowercase or uppercase

---
+++ JavaCodes/Typecheck.java
@@ -0,0 +1,16 @@
+package vishwa;
+
+import java.util.Scanner;
+
+public class Typecheck {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        char ch=in.next().trim().charAt(0);
+        if(ch>='a' && ch<='z'){
+            System.out.println("Lowercase");
+        }else{
+            System.out.println("Uppercase");
+        }
+
+    }
+}

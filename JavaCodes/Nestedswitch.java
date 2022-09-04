2fb957758848bb933577d518f671ebeb02391722
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2022-09-04 12:35:00 +0530

    Nested Switch

---
+++ JavaCodes/Nestedswitch.java
@@ -0,0 +1,51 @@
+package vishwa;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Nestedswitch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int empID=in.nextInt();
+        String dept=in.next();
+        /*
+        switch(empID){
+            case 1:
+                System.out.println("Vishwa");
+                break;
+            case 2:
+                System.out.println("Kunal kushwaha");
+                break;
+            case 3:
+                System.out.println("Emp no.3");
+                switch (dept){
+                    case "IT":
+                        System.out.println("It dept.");
+                        break;
+                    case "Ece":
+                        System.out.println("Ece dept.");
+                        break;
+                    default:
+                        System.out.println("Enter a valid dept.");
+                }
+                break;
+            default:
+                System.out.println("Enter a valid empID");
+        }
+        */
+        //New Syntax.
+        switch (empID) {
+            case 1 -> System.out.println("Vishwa");
+            case 2 -> System.out.println("Kunal kushwaha");
+            case 3 -> {
+                System.out.println("Emp no.3");
+                switch (dept) {
+                    case "IT" -> System.out.println("It dept.");
+                    case "Ece" -> System.out.println("Ece dept.");
+                    default -> System.out.println("Enter a valid dept.");
+                }
+            }
+            default -> System.out.println("Enter a valid empID");
+        }
+    }
+}

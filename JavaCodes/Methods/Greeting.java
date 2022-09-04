f71052eb9f662006e2de67b04ce77630c4dd21b2
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2022-09-04 17:47:37 +0530

    No Return Type (String)

---
+++ JavaCodes/Methods/Greeting.java
@@ -0,0 +1,10 @@
+package vishwa;
+
+public class Greeting {
+    public static void main(String[] args) {
+        greet();
+    }
+    static void greet(){
+        System.out.println("Hi! Welcome to DeveloperCommunity!");
+    }
+}

0a7f860c2f6f5a36eec42a1655ea258b108ec402
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2022-09-04 17:48:28 +0530

    Example for Return_Type

---
+++ JavaCodes/Methods/Returntype_example.java
@@ -0,0 +1,28 @@
+package vishwa;
+
+import java.util.Scanner;
+
+public class Returntype_example {
+    public static void main(String[] args) {
+        String res=greet();
+        System.out.println(res);
+        int ans=sum();
+        System.out.println(ans);
+    }
+    //IntegerType
+    static int sum(){
+        Scanner in=new Scanner(System.in);
+
+        System.out.print("Enter 1st number:");
+        int n1=in.nextInt();
+        System.out.print("Enter 2nd number:");
+        int n2=in.nextInt();
+        int sum=n1+n2;
+        return sum;
+    }
+    //StringType
+    static String greet(){
+        String greeting="Welcome Developers!";
+        return greeting;
+    }
+}

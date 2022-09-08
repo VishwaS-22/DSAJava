619055d7816c8faed526154fda7a81d5e5a13e6e
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2022-09-08 15:56:49 +0530

    OverloadingExample

---
+++ JavaCodes/Methods/Overloading.java
@@ -0,0 +1,32 @@
+package vishwa;
+import java.util.*;
+public class Overloading { //Means having same name for multiple func. ,but it can exists when parameters are of diff. type.
+    public static void main(String[] args) {
+        //func(11);
+        //func("Vishwa");
+        //int ans=sum(5,6);
+        //System.out.println(ans);
+        varargs(1,2,3);
+        varargs("Vishwa_sk","KunalKushwaha","Saiyam Pathak");
+
+    }
+    static void varargs(int ...s){
+        System.out.println(Arrays.toString(s));
+    }
+    static void varargs(String ...k){
+        System.out.println(Arrays.toString(k));
+    }
+    static int sum(int a,int b){
+        return a+b;
+    }
+    static int sum(int a,int b,int c){
+        return a+b+c;
+    }
+
+    static void func(int a){
+        System.out.println(a);
+    }
+    static void func(String v){
+        System.out.println(v);
+    }
+}

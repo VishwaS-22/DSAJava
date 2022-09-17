125c0bb9e0b153ca54924651df3cfc5f9369d64b
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2022-09-17 08:59:37 +0530

    OverloadingExample

--- JavaCodes/Methods/Overloading.java
+++ JavaCodes/Methods/Overloading.java
@@ -1,6 +1,7 @@
 package vishwa;
 import java.util.*;
-public class Overloading { //Means having same name for multiple func. ,but it can exists when parameters are of diff. type.
+public class Overloading { //Means having same name for multiple func. ,but it can exists when parameters are of diff. type
+                           //& also checks no. of arguements we are passing, it take which func it wants(refer no. 8 with 20).
     public static void main(String[] args) {
         //func(11);
         //func("Vishwa");

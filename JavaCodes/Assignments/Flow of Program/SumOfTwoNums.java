9fee180af00e809d6757c8022a6c69136a57457e
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2022-09-27 21:56:02 +0530

    SumOfTwoIntegers

---
+++ JavaCodes/Assignments/Flow of Program/SumOfTwoNums.java
@@ -0,0 +1,14 @@
+package vishwa;
+import java.util.*;
+import static java.lang.System.*;
+
+public class SumOfTwoNums
+{
+    public static void main(String[] args)
+    {
+        var in = new Scanner(System.in);
+        var a=in.nextInt();
+        var b=in.nextInt();
+        out.print(a+b);
+    }
+}

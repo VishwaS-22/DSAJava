a6a1c0d0af74fef303e6a0d3e80cd3055fb224a6
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2022-09-28 23:48:05 +0530

    LcmOf2Nums

---
+++ JavaCodes/Assignments/FlowOfProgram/LCM.java
@@ -0,0 +1,23 @@
+package vishwa;
+import java.util.*;
+public class LCM {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n1=in.nextInt();
+        int n2=in.nextInt();
+        //int n1=15;
+        //int n2=20;
+        int res=lcm(n1,n2);
+        System.out.println(res);
+        //int res1=gcd(n1,n2);
+        //System.out.println(res1);
+    }
+    static int lcm(int a,int b){
+        return (a/gcd(a,b))*b;
+    }
+    static int gcd(int m,int n){
+        if(m==0)
+            return n;
+        return gcd(n%m,m);
+    }
+}

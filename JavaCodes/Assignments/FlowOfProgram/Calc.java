663c94cec02d6d7d1aa3ab1e05e8a98a996eb70e
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2022-10-03 11:12:36 -0700

    Basic Calc

---
+++ JavaCodes/Assignments/FlowOfProgram/Calc.java
@@ -0,0 +1,16 @@
+// Calculator.
+import java.util.*;
+class Calc{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        char ch=in.next().charAt(0);
+        switch(ch){
+            case '+': System.out.println(a+b);
+            case '-': System.out.println(a-b);
+            case '*': System.out.println(a*b);
+            case '/': System.out.println(a/b);
+        }
+    }
+}

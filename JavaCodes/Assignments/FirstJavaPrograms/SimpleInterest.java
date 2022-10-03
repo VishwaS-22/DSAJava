ac72db17a57f1eae20ab72ab3217803fae1424e6
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2022-10-03 11:17:08 -0700

    SI Problem

---
+++ JavaCodes/Assignments/FirstJavaPrograms/SimpleInterest.java
@@ -0,0 +1,13 @@
+// Finding Simple Interest.
+import java.util.*;
+class SimpleInterest{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int p=in.nextInt();
+        int t=in.nextInt();
+        int r=in.nextInt();
+        int res;
+        res=(p*t*r)/100;
+        System.out.println(res);
+    }
+}

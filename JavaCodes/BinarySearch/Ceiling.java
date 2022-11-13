0ba03e3f75d9ca327d9dcb5d26aa3b634279324b
unknown <VishwaS-22@github.com>
2022-11-13 16:11:13 +0530

    BinarySearch Problems

---
+++ JavaCodes/BinarySearch/Ceiling.java
@@ -0,0 +1,35 @@
+package vishwa;
+
+public class Ceiling {
+    public static void main(String[] args)
+    {
+        int[] arr1={2,3,4,9,14,16,18}; //sorted array.
+        int searchElement=15;
+        int res=ceilFunc(arr1,searchElement);
+        System.out.print(res);
+    }
+    //func to return index of the smallest number that is greater than target.
+    static int ceilFunc(int arr[],int target) {
+        int start = 0;
+
+        int end = arr.length - 1;
+
+        if(target>arr[arr.length-1]){
+            return -1;
+        }
+
+
+        while (start <= end) {
+            //int mid = (start + end)/ 2;//might be possible that it exceeds the range of int, so we can use alter.
+            int mid = start + (end - start) / 2; //same
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}

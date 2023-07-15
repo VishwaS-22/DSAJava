1e5d7ed26d6c8b414865c707ba218a7a24f95e37
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2023-07-15 22:40:20 +0530

    Basic Problems

---
+++ JavaCodes/Assignments/LoopsAndConditionals/PerimeterOfEquilateral.java
@@ -0,0 +1,9 @@
+package vishwa;
+import java.util.*;
+public class PerimeterOfEquilateral {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        int s = ip.nextInt();
+        System.out.println(3*s);
+    }
+}
